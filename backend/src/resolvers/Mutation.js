78944f2307b75c078e2cbc35016a568b4074d169
Adam Berkowitz <aberkow@Adams-MacBook-Pro.local>
2019-01-02 22:13:48 -0500

    setting up basic queries, mutation, etc...

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -7,6 +7,50 @@ const Mutation = {
       }
     }, info)
     return user
+  },
+  updateUser: async (parent, args, ctx, info) => {
+    const updates = {...args};
+
+    delete updates.id;
+
+    const updatedUser = await ctx.db.mutation.updateUser({
+      data: updates,
+      where: { id: args.id }
+    }, info)
+    return updatedUser;
+  },
+  deleteUser: async (parent, args, ctx, info) => {
+    const deletedUser = await ctx.db.mutation.deleteUser({
+      where: { id: args.id }
+    }, info);
+    return deletedUser;
+  },
+  createTechnique: async (parent, args, ctx, info) => {
+    const technique = await ctx.db.mutation.createTechnique({
+      data: {
+        name: args.name.toLowerCase(),
+        description: args.description,
+        rank: args.rank
+      }
+    }, info)
+    return technique
+  },
+  updateTechnique: async (parent, args, ctx, info) => {
+    const updates = { ...args }
+
+    delete updates.id
+
+    const updatedTechnique = await ctx.db.mutation.updateTechnique({
+      data: updates,
+      where: { id: args.id }
+    }, info)
+
+    return updatedTechnique
+  },
+  deleteTechnique: async (parent, args, ctx, info) => {
+    return ctx.db.mutation.deleteTechnique({
+      where: { id: args.id }
+    }, info)
   }
 }
 
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,7 +1,10 @@
 const { forwardTo } = require('prisma-binding');
 
 const Query = {
-  users: forwardTo('db')
+  users: forwardTo('db'),
+  techniques: forwardTo('db'),
+  combinations: forwardTo('db'),
+  technique: forwardTo('db')
 }
 
 module.exports = Query;
